633e994a0beedee0df157f0eed46099dcaa5a344
vikas <vikas.kumar123@ssipmt.com>
2024-03-20 23:21:39 +0530

    error solve in edit button

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const port = 8080;
 const path = require("path");
 const app = express();
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wandurlust");
+}
+
 main()
   .then(() => {
     console.log("connected to db");
@@ -23,10 +29,6 @@ main()
   .catch((err) => {
     console.log(err);
   });
-
-async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wandurlust");
-}
 
 app.get("/", (req, res) => {
   res.redirect("/listings");
@@ -41,6 +43,7 @@ const validateListing = (req, res, next) => {
     next();
   }
 };
+
 const validateReviews = (req, res, next) => {
   let { error } = reviewSchema.validate(req.body);
   if (error) {
@@ -58,7 +61,6 @@ app.get(
     res.render("listings/index.ejs", { allListings });
   })
 );
-//new route
 
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
@@ -68,42 +70,38 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
   })
 );
 
-
-//create route
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    let result = listingSchema.validate(req.body);
-    console.log(result);
     const newListing = new Listing(req.body.listing);
-
     await newListing.save();
     res.redirect("/listings");
   })
 );
 
-//edit route
-
 app.get(
-  "/listing/:id/edit",
+  "/listings/:id/edit",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-
-    res.render("listings/edit.ejs", { listing });
+    // res.send("it working")
+    res.render("listings/edit.ejs",{ listing })
   })
 );
+
+
 
 app.put(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing });
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
   })
 );
@@ -124,50 +122,30 @@ app.post(
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
-
     console.log("newReview send ");
     res.redirect(`/listings/${listing._id}`);
   })
 );
 
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  })
+);
 
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(
-async(req,res)=>{
-
-  let {id,reviewId}=req.params;
-  await Listing.findByIdAndUpdate(id,{ $pull : {reviews:reviewId}}); 
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`)
-}
-
-))
-
-// app.get("/testListing",async (req,res)=>{
-// let sampleListing=new Listing({
-// title:"my new Villa",
-// description:"by the beach",
-// price:"1200",
-// location:"mumbai",
-// country:"India"
-// });
-
-// await sampleListing.save();
-// console.log("sample was send");
-// res.send("successfull testing");
-
-// });
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "page not found "));
 });
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something Went Wrong" } = err;
-  // res.status(statusCode).send(message);
   res.status(statusCode).render("error.ejs", { message });
 });
 
