786375d88372c91eb73beab7f0932cfd0339b263
vikas <vikas.kumar123@ssipmt.com>
2024-03-20 16:43:51 +0530

    ok ok before routing

--- models/listing.js
+++ models/listing.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { type } = require("../schema");
 const listingSchema = require("../schema");
 const Schema = mongoose.Schema;
-const Review = require("./review.js");
+const Review=require("./review.js")
 const listeningSchema = new Schema({
   title: {
     type: String,
@@ -10,19 +10,19 @@ const listeningSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
+    type: String
   },
   price: Number,
   location: String,
   country: String,
-  reviews: [{ type: Schema.Types.ObjectId }],
+  reviews:[
+    {type:Schema.Types.ObjectId,
+    
+    }
+  ]
 });
 
-listingSchema.POST("findOneAndDelete", async (listing) => {
-  if (listing) {
-    await Review.deleteMany({ _id: { $in: listing.reviews } });
-  }
-});
+
 
 const Listing = mongoose.model("Listing", listeningSchema);
 
